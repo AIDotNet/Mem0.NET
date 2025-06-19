4480f583b4df564f300ad7a53cbb45191b82eda7
token <239573049@qq.com>
2025-06-20 01:27:12 +0800

    更新 .gitignore 文件以排除开发环境文件，添加 docker-compose.yaml 和 .env.example 文件以支持容器化部署

--- samples/Mem0Console/Program.cs
+++ samples/Mem0Console/Program.cs
@@ -1,30 +1,29 @@
 ﻿using Mem0.NET;
 
-var client = new Mem0Client("", "http://0.0.0.0:8000/");
+var client = new Mem0Client("", "http://127.0.0.1:8000/");
 
-await client.AddAsync(new List<Message>()
-{
-    new Message()
+await client.AddAsync([
+    new()
     {
         Role = "user",
         Content = "Hi, I'm Alex. I'm a vegetarian and I'm allergic to nuts."
     },
-    new Message()
+
+    new()
     {
         Role = "assistant",
         Content =
             "Hello Alex! I've noted that you're a vegetarian and have a nut allergy. I'll keep this in mind for any food-related recommendations or discussions."
     }
-}, "alex");
+], "alex");
 
-var query = "What can I cook for dinner tonight?";
 var response = await client.SearchAsync(new SearchRequest()
 {
-    Query = query,
+    Query = "What can I cook for dinner tonight?",
     UserId = "alex"
 });
 
-foreach (var memory in response.results)
+foreach (var memory in response.Results)
 {
     Console.WriteLine($"Memory ID: {memory.Content}");
 }
--- src/Mem0.NET/Model/Memory.cs
+++ src/Mem0.NET/Model/Memory.cs
@@ -8,23 +8,47 @@ namespace Mem0.NET;
 public class Memory
 {
     [JsonPropertyName("id")] public string Id { get; set; } = string.Empty;
+    
     [JsonPropertyName("memory")] public string Content { get; set; } = string.Empty;
+    
     [JsonPropertyName("text")] public string Text { get; set; } = string.Empty;
+    
     [JsonPropertyName("user_id")] public string? UserId { get; set; }
+    
     [JsonPropertyName("agent_id")] public string? AgentId { get; set; }
+    
     [JsonPropertyName("app_id")] public string? AppId { get; set; }
+    
     [JsonPropertyName("run_id")] public string? RunId { get; set; }
+    
     [JsonPropertyName("metadata")] public Dictionary<string, object>? Metadata { get; set; }
+    
     [JsonPropertyName("created_at")] public DateTime? CreatedAt { get; set; }
+    
     [JsonPropertyName("updated_at")] public DateTime? UpdatedAt { get; set; }
+    
     [JsonPropertyName("score")] public double? Score { get; set; }
 }
 
 
 public class SearchResult
 {
-    public Memory[] results { get; set; }
+    [JsonPropertyName("results")]
+    public Memory[] Results { get; set; }
     
-    public object[] relations { get; set; }
+    [JsonPropertyName("relations")]
+    public Relations[] Relations { get; set; }
+}
+
+public class Relations
+{
+    [JsonPropertyName("source")]
+    public string Source { get; set; }
+    
+    [JsonPropertyName("relationship")]
+    public string Relationship { get; set; }
+    
+    [JsonPropertyName("destination")]
+    public string Destination { get; set; }
 }
 
